d3569d6997a60a6c064633e3551c8de5f3a16308
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-10-26 08:55:22 -0400

    [5B] v2.0
    (Debug) Changed nextLine to nextInt for both int variables using the scanner

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,4 +1,4 @@
-// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v1.5
+// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v2.0
 
 import java.util.Scanner;
 
@@ -8,13 +8,13 @@ public class DamageDealt {
 
         System.out.println("How many attacks will you do? ");
 
-        int attackNum = myScanner.nextLine();
+        int attackNum = myScanner.nextInt();
 
         System.out.println(" ");
 
         System.out.println("How much damage will each attack do? ");
 
-        int attackPower = myScanner.nextLine();
+        int attackPower = myScanner.nextInt();
 
         System.out.println(" ");
 
