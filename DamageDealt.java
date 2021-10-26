bbab6f970b84e815d8d0fd86cfaa779befc78584
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-10-26 09:29:32 -0400

    [5B] v3.5
    Fixed code skipping

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,10 +1,22 @@
-// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v3.0
+// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v3.5
 
 import java.util.Scanner;
 
 public class DamageDealt {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What is getting hit? ");
+
+        String enemy = myScanner.nextLine();
+
+        System.out.println(" ");
+
+        System.out.println("What is it getting hit with? ");
+
+        String weapon = myScanner.nextLine();
+
+        System.out.println(" ");
 
         System.out.println("How many attacks will you do? ");
 
@@ -15,19 +27,7 @@ public class DamageDealt {
         System.out.println("How much damage will each attack do? ");
 
         int attackPower = myScanner.nextInt();
-
-        System.out.println(" ");
-
-        System.out.println("What is getting hit? ");
-
-        String enemy = myScanner.nextLine();
         
-        System.out.println(" ");
-
-        System.out.println("What is it getting hit with? ");
-
-        String weapon = myScanner.nextLine();
-
         System.out.println(" ");
 
         int totalDmg = attackNum * attackPower;
