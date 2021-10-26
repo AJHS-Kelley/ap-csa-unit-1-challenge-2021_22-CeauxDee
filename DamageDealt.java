e613ab2002270958c674662f885ec71078e12d7c
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-10-26 09:03:57 -0400

    [5B] v3.0
    Added damage calculation and display

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,4 +1,4 @@
-// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v2.5
+// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v3.0
 
 import java.util.Scanner;
 
@@ -27,5 +27,15 @@ public class DamageDealt {
         System.out.println("What is it getting hit with? ");
 
         String weapon = myScanner.nextLine();
+
+        System.out.println(" ");
+
+        int totalDmg = attackNum * attackPower;
+
+        double playerDps = totalDmg / 4.0;
+
+        System.out.println("You have dealt " + totalDmg + " with the " + weapon + " to the " + enemy + ".");
+
+        System.out.println("You dealt " + playerDps + " damage per second with the " + weapon + " to the " + enemy + ".");
     }
 }
