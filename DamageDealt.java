3a70a5f6e40257a3b086a18336dd56b839c5883b
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-10-26 08:45:14 -0400

    [5B] v0.0
    Imported scanner and created static void

---
+++ DamageDealt.java
@@ -0,0 +1,11 @@
+// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v0.0
+
+import java.util.Scanner;
+
+public class Unit1Challenge {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("How many attacks will you do? ");
+    }
+}
