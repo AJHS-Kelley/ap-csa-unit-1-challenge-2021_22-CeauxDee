18739d5be9b63fb09077f2e10ad5494ded108b10
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-10-26 08:45:52 -0400

    [5B] v0.5
    Changed class name

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,8 +1,8 @@
-// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v0.0
+// Cody Collins, AP CSA Uni 1 Challenge, Andrew Jackson High School, 10/26/2021, 8:36 am, v0.5
 
 import java.util.Scanner;
 
-public class Unit1Challenge {
+public class DamageDealt {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
